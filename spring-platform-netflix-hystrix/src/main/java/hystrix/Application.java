38cc8a7688ff59e532ba7d1b8a4916cc8660a7f7
Spencer Gibb <spencer@gibb.us>
2014-07-11 17:50:33 -0600

    added hystrix-dashboard

---
+++ spring-platform-netflix-hystrix/src/main/java/hystrix/Application.java
@@ -0,0 +1,39 @@
+package hystrix;
+
+import hystrix.stream.MockStreamServlet;
+import hystrix.stream.ProxyStreamServlet;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by sgibb on 7/11/14.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class).web(true);
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @Bean
+    public ServletRegistrationBean mockStreamServlet() {
+        return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
+    }
+
+    @Bean
+    public ServletRegistrationBean proxyStreamServlet() {
+        return new ServletRegistrationBean(new ProxyStreamServlet(), "/proxy.stream");
+    }
+}
