5ee8c8c8462a83fc0646ce7044f9e28a4609ccb4
Dave Syer <dsyer@gopivotal.com>
2014-07-11 16:46:38 +0100

    Initial version with client autoconfig

---
+++ src/main/java/org/springframework/platform/netflix/eureka/EurekaClientAutoConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.platform.netflix.eureka;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo.InstanceStatus;
+import com.netflix.discovery.DiscoveryManager;
+
+/**
+ * @author Dave Syer
+ *
+ */
+@Configuration
+@EnableConfigurationProperties({EurekaClientConfigBean.class, EurekaInstanceConfigBean.class})
+public class EurekaClientAutoConfiguration implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	private EurekaClientConfigBean clientConfig;
+	@Autowired
+	private EurekaInstanceConfigBean instanceConfig;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		ApplicationInfoManager.getInstance().setInstanceStatus(InstanceStatus.UP);		
+	}
+
+	@PostConstruct
+	public void init() {
+		DiscoveryManager.getInstance().initComponent(
+				instanceConfig,
+				clientConfig);
+	}
+	
+	@PreDestroy
+	public void close() {
+		DiscoveryManager.getInstance().shutdownComponent();
+	}
+
+}
---
+++ src/test/java/org/springframework/platform/netflix/eureka/sample/Application.java
@@ -0,0 +1,25 @@
+package org.springframework.platform.netflix.eureka.sample;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
+	}
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+}
