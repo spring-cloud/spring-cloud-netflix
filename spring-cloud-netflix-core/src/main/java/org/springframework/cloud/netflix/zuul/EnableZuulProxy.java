6425862477e96d867854504f6503cfe6ec05aded
Dave Syer <dsyer@pivotal.io>
2014-09-05 12:39:57 +0100

    Make @EnableZuulProxy switch on Hystrix

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/EnableZuulProxy.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/EnableZuulProxy.java
@@ -4,10 +4,12 @@ import java.lang.annotation.*;
 
 import org.springframework.context.annotation.Import;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author Spencer Gibb
  */
+@EnableHystrix
 @EnableEurekaClient
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
