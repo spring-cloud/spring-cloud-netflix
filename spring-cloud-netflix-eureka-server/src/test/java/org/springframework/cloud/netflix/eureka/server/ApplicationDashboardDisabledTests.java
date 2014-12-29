4e6261e67bf5c006658004b72fb0d2ed3f0e76f5
Julien Roy <roy@fullsix.com>
2014-12-29 23:35:35 +0100

    Eureka dashboard controller path mapping can be configured or disabled

---
+++ spring-cloud-netflix-eureka-server/src/test/java/org/springframework/cloud/netflix/eureka/server/ApplicationDashboardDisabledTests.java
@@ -0,0 +1,44 @@
+package org.springframework.cloud.netflix.eureka.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.cloud.netflix.eureka.server.ApplicationContextTests.Application;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({ "server.port=0", "spring.application.name=eureka",
+		"eureka.dashboard.enabled=false" })
+public class ApplicationDashboardDisabledTests {
+
+	@Value("${local.server.port}")
+	private int port = 0;
+
+	@Test
+	public void catalogLoads() {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/eureka/apps", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void dashboardLoads() {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/", String.class);
+		assertEquals(HttpStatus.NOT_FOUND, entity.getStatusCode());
+	}
+
+}
