96335e1d244f07b8aaf440867879e175b7e27278
Dave Syer <dsyer@pivotal.io>
2015-02-09 17:00:22 +0000

    Fix broken test

--- spring-cloud-netflix-sidecar/src/test/java/org/springframework/cloud/netflix/sidecar/SidecarApplicationTests.java
+++ spring-cloud-netflix-sidecar/src/test/java/org/springframework/cloud/netflix/sidecar/SidecarApplicationTests.java
@@ -21,10 +21,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SidecarApplication.class)
 @IntegrationTest("server.port=0")
+@WebAppConfiguration
 public class SidecarApplicationTests {
 
 	@Test
