spring-cloud/spring-cloud-netflix

commit 18b346def151bd9182a7dfc8638077faaced0183

update the serviceId and location for statically configured routes that are also part of serviceDiscovery.
fixes gh-199

spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/ProxyRouteLocator.java
@@ -153,16 +153,17 @@ public class ProxyRouteLocator implements RouteLocator {
 				// Ignore specifically ignored services and those that were manually
 				// configured
 				String key = "/" + serviceId + "/**";
-				ZuulRoute route = new ZuulRoute(key, serviceId);
 				if (staticServices.containsKey(serviceId)
 						&& staticServices.get(serviceId).getUrl() == null) {
 					// Explicitly configured with no URL, cannot be ignored
-					routesMap.put(key, route);
+					// all static routes are already in routesMap, just update
+					ZuulRoute staticRoute = staticServices.get(serviceId);
+					staticRoute.updateRoute(key, serviceId);
 				}
 				if (!PatternMatchUtils.simpleMatch(ignored, serviceId)
 						&& !routesMap.containsKey(key)) {
 					// Not ignored
-					routesMap.put(key, route);
+					routesMap.put(key, new ZuulRoute(key, serviceId));
 				}
 			}
 		}

spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/ZuulProperties.java
@@ -102,6 +102,10 @@ public class ZuulProperties {
 		}
 
 		public ZuulRoute(String path, String location) {
+			updateRoute(path, location);
+		}
+
+		public void updateRoute(String path, String location) {
 			this.id = extractId(path);
 			this.path = path;
 			setLocation(location);

spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/zuul/filters/ProxyRouteLocatorTests.java
@@ -17,6 +17,7 @@
 package org.springframework.cloud.netflix.zuul.filters;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.junit.Before;
@@ -31,6 +32,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -255,6 +257,23 @@ public class ProxyRouteLocatorTests {
 		Map<String, String> routesMap = routeLocator.getRoutes();
 		String serviceId = routesMap.get(getMapping("foo"));
 		assertNotNull("routes ignored foo", serviceId);
+	}
+
+	@Test
+	public void testIgnoredRoutePropertiesRemain() {
+		ZuulRoute route = new ZuulRoute("/foo/**");
+		route.setStripPrefix(true);
+		route.setRetryable(Boolean.TRUE);
+		this.properties.getRoutes().put("foo", route);
+		ProxyRouteLocator routeLocator = new ProxyRouteLocator("/", this.discovery,
+				this.properties);
+		this.properties.setIgnoredServices(Collections.singletonList("*"));
+		given(this.discovery.getServices()).willReturn(Collections.singletonList("foo"));
+		LinkedHashMap<String, ZuulRoute> routes = routeLocator.locateRoutes();
+		ZuulRoute actual = routes.get(getMapping("foo"));
+		assertNotNull("routes ignored foo", actual);
+		assertTrue("stripPrefix is wrong", actual.isStripPrefix());
+		assertEquals("retryable is wrong", Boolean.TRUE, actual.getRetryable());
 	}
 
 	@Test

commit 24a06ca630b5e0351359430d6ee2656426d1f924

only update route location if location is empty.  Fixes bug introduced in 18b346def151bd9182a7dfc8638077faaced0183

spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/ProxyRouteLocator.java
@@ -156,9 +156,12 @@ public class ProxyRouteLocator implements RouteLocator {
 				if (staticServices.containsKey(serviceId)
 						&& staticServices.get(serviceId).getUrl() == null) {
 					// Explicitly configured with no URL, cannot be ignored
-					// all static routes are already in routesMap, just update
+					// all static routes are already in routesMap
+					// Update location using serviceId if location is null
 					ZuulRoute staticRoute = staticServices.get(serviceId);
-					staticRoute.updateRoute(key, serviceId);
+					if (!StringUtils.hasText(staticRoute.getLocation())) {
+						staticRoute.setLocation(serviceId);
+					}
 				}
 				if (!PatternMatchUtils.simpleMatch(ignored, serviceId)
 						&& !routesMap.containsKey(key)) {

spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/ZuulProperties.java
@@ -102,10 +102,6 @@ public class ZuulProperties {
 		}
 
 		public ZuulRoute(String path, String location) {
-			updateRoute(path, location);
-		}
-
-		public void updateRoute(String path, String location) {
 			this.id = extractId(path);
 			this.path = path;
 			setLocation(location);

spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/zuul/filters/ProxyRouteLocatorTests.java
@@ -277,6 +277,25 @@ public class ProxyRouteLocatorTests {
 	}
 
 	@Test
+	public void testIgnoredRouteNonServiceIdPathRemains() {
+		//This is how you setup a route defined like zuul.proxy.route.foo=/**
+		ZuulRoute route = new ZuulRoute("/**", "foo");
+		route.setId("foo");
+
+		this.properties.getRoutes().put("foo", route);
+		ProxyRouteLocator routeLocator = new ProxyRouteLocator("/", this.discovery,
+				this.properties);
+		this.properties.setIgnoredServices(Collections.singletonList("*"));
+		given(this.discovery.getServices()).willReturn(Collections.singletonList("foo"));
+		LinkedHashMap<String, ZuulRoute> routes = routeLocator.locateRoutes();
+		ZuulRoute actual = routes.get("/**");
+		assertNotNull("routes ignored foo", actual);
+		assertEquals("id is wrong", "foo", actual.getId());
+		assertEquals("location is wrong", "foo", actual.getServiceId());
+		assertEquals("path is wrong", "/**", actual.getPath());
+	}
+
+	@Test
 	public void testIgnoredRouteIncludedIfConfiguredAndNotDiscovered() {
 		this.properties.getRoutes()
 				.put("foo", new ZuulRoute("/foo/**", "http://foo.com"));