408910e893b8dd8d58afbf336dd20d211ce75425
Roy Clarkson <rclarkson@pivotal.io>
2015-04-08 14:06:41 -0500

    Ignore 'Connection: close' header from stream response
    This commit resolves an issue where some versions of Cloud Foundry are
    setting the 'Connection: close' HTTP header in the Hystrix stream
    response, which causes the dashboard to not display the stream data.
    
    See https://github.com/cloudfoundry/gorouter/issues/71

---
+++ spring-cloud-netflix-hystrix-dashboard/src/main/java/org/springframework/cloud/netflix/hystrix/dashboard/HystrixDashboardProperties.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.netflix.hystrix.dashboard;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Roy Clarkson
+ */
+@ConfigurationProperties("hystrix.dashboard")
+public class HystrixDashboardProperties {
+
+	/**
+	 * Directs the Hystrix dashboard to ignore 'Connection:close' headers if present in
+	 * the Hystrix response stream
+	 */
+	private boolean enableIgnoreConnectionCloseHeader = false;
+
+	public boolean isEnableIgnoreConnectionCloseHeader() {
+		return enableIgnoreConnectionCloseHeader;
+	}
+
+	public void setEnableIgnoreConnectionCloseHeader(
+			boolean enableIgnoreConnectionCloseHeader) {
+		this.enableIgnoreConnectionCloseHeader = enableIgnoreConnectionCloseHeader;
+	}
+
+}
