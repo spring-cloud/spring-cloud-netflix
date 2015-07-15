42abed3fcb27b1d1ab3e38ed3089d0a7b7537c14
Spencer Gibb <spencer@gibb.us>
2015-07-15 14:05:13 -0600

    Add support for using Apache HttpClient with Feign
    fixes gh-415

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/support/ResponseEntityDecoder.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/support/ResponseEntityDecoder.java
@@ -1,64 +1,61 @@
-
 package org.springframework.cloud.netflix.feign.support;
-
-import feign.FeignException;
-import feign.Response;
-import feign.codec.Decoder;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.LinkedList;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import feign.FeignException;
+import feign.Response;
+import feign.codec.Decoder;
+
 /**
- * Decoder adds compatibility for Spring MVC's ResponseEntity to any
- * other decoder via composition.
+ * Decoder adds compatibility for Spring MVC's ResponseEntity to any other decoder via
+ * composition.
  * @author chadjaros
  */
 @Slf4j
 public class ResponseEntityDecoder implements Decoder {
 
-    private Decoder decoder;
+	private Decoder decoder;
 
-    public ResponseEntityDecoder(Decoder decoder) {
-        this.decoder = decoder;
-    }
+	public ResponseEntityDecoder(Decoder decoder) {
+		this.decoder = decoder;
+	}
 
-    @Override
-    public Object decode(final Response response, Type type) throws IOException,
-            FeignException {
+	@Override
+	public Object decode(final Response response, Type type) throws IOException,
+			FeignException {
 
-        if(type instanceof ParameterizedType &&
-                ((ParameterizedType) type).getRawType().equals(ResponseEntity.class)) {
+		if (type instanceof ParameterizedType
+				&& ((ParameterizedType) type).getRawType().equals(ResponseEntity.class)) {
 
-            type = ((ParameterizedType) type).getActualTypeArguments()[0];
-            Object decodedObject = decoder.decode(response, type);
+			type = ((ParameterizedType) type).getActualTypeArguments()[0];
+			Object decodedObject = decoder.decode(response, type);
 
-            return createResponse(
-                    decodedObject.getClass(),
-                    decodedObject,
-                    response);
-        }
-        else {
-            return decoder.decode(response, type);
-        }
-    }
+			return createResponse(decodedObject, response);
+		}
+		else {
+			return decoder.decode(response, type);
+		}
+	}
 
-    private <T> ResponseEntity<T> createResponse(Class<T> clazz, Object instance, Response response) {
+	@SuppressWarnings("unchecked")
+	private <T> ResponseEntity<T> createResponse(Object instance, Response response) {
 
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        for(String key: response.headers().keySet()) {
-            headers.put(key, new LinkedList<>(response.headers().get(key)));
-        }
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+		for (String key : response.headers().keySet()) {
+			headers.put(key, new LinkedList<>(response.headers().get(key)));
+		}
 
-        return new ResponseEntity<T>(
-                clazz.cast(instance),
-                headers,
-                HttpStatus.valueOf(response.status()));
-    }
+		return new ResponseEntity<>((T) instance, headers, HttpStatus.valueOf(response
+				.status()));
+	}
 }
