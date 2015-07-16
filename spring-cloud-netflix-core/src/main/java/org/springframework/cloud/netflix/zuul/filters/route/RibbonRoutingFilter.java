b494e1d2cc8249afb793bbb6a253a482f4420c61
Spencer Gibb <spencer@gibb.us>
2015-07-15 20:03:49 -0600

    refactor RibbonCommand to an interface and factory
    lays the groundworkd to implement an httpclient ribbon command
    
    fixes gh-412

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/route/RibbonRoutingFilter.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/zuul/filters/route/RibbonRoutingFilter.java
@@ -18,26 +18,19 @@ package org.springframework.cloud.netflix.zuul.filters.route;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import lombok.extern.apachecommons.CommonsLog;
 
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
-import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.MultiValueMap;
 
 import com.netflix.client.ClientException;
-import com.netflix.client.http.HttpRequest.Verb;
-import com.netflix.client.http.HttpResponse;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
-import com.netflix.niws.client.http.RestClient;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -45,20 +38,17 @@ import com.netflix.zuul.exception.ZuulException;
 @CommonsLog
 public class RibbonRoutingFilter extends ZuulFilter {
 
-	public static final String CONTENT_ENCODING = "Content-Encoding";
-
-	private SpringClientFactory clientFactory;
-
 	private ProxyRequestHelper helper;
+	private RibbonCommandFactory<?> ribbonCommandFactory;
 
 	public RibbonRoutingFilter(ProxyRequestHelper helper,
-			SpringClientFactory clientFactory) {
+			RibbonCommandFactory<?> ribbonCommandFactory) {
 		this.helper = helper;
-		this.clientFactory = clientFactory;
+		this.ribbonCommandFactory = ribbonCommandFactory;
 	}
 
-	public RibbonRoutingFilter(SpringClientFactory clientFactory) {
-		this(new ProxyRequestHelper(), clientFactory);
+	public RibbonRoutingFilter(RibbonCommandFactory<?> ribbonCommandFactory) {
+		this(new ProxyRequestHelper(), ribbonCommandFactory);
 	}
 
 	@Override
@@ -81,29 +71,9 @@ public class RibbonRoutingFilter extends ZuulFilter {
 	@Override
 	public Object run() {
 		RequestContext context = RequestContext.getCurrentContext();
-		HttpServletRequest request = context.getRequest();
-
-		MultiValueMap<String, String> headers = this.helper
-				.buildZuulRequestHeaders(request);
-		MultiValueMap<String, String> params = this.helper
-				.buildZuulRequestQueryParams(request);
-		Verb verb = getVerb(request);
-		InputStream requestEntity = getRequestBody(request);
-
-		String serviceId = (String) context.get("serviceId");
-		Boolean retryable = (Boolean) context.get("retryable");
-
-		RestClient restClient = this.clientFactory.getClient(serviceId, RestClient.class);
-
-		String uri = this.helper.buildZuulRequestURI(request);
-
-		// remove double slashes
-		uri = uri.replace("//", "/");
-		String service = (String) context.get("serviceId");
-
 		try {
-			HttpResponse response = forward(restClient, service, verb, uri, retryable, headers, params,
-					requestEntity);
+			RibbonCommandContext commandContext = buildCommandContext(context);
+			ClientHttpResponse response = forward(commandContext);
 			setResponse(response);
 			return response;
 		}
@@ -114,17 +84,37 @@ public class RibbonRoutingFilter extends ZuulFilter {
 		return null;
 	}
 
-	private HttpResponse forward(RestClient restClient, String service, Verb verb, String uri, Boolean retryable,
-			MultiValueMap<String, String> headers, MultiValueMap<String, String> params,
-			InputStream requestEntity) throws Exception {
-		Map<String, Object> info = this.helper.debug(verb.verb(), uri, headers, params,
-				requestEntity);
-		RibbonCommand command = new RibbonCommand(service, restClient, verb, uri, retryable,
+	private RibbonCommandContext buildCommandContext(RequestContext context) {
+		HttpServletRequest request = context.getRequest();
+
+		MultiValueMap<String, String> headers = this.helper
+				.buildZuulRequestHeaders(request);
+		MultiValueMap<String, String> params = this.helper
+				.buildZuulRequestQueryParams(request);
+		String verb = getVerb(request);
+		InputStream requestEntity = getRequestBody(request);
+
+		String serviceId = (String) context.get("serviceId");
+		Boolean retryable = (Boolean) context.get("retryable");
+
+		String uri = this.helper.buildZuulRequestURI(request);
+
+		// remove double slashes
+		uri = uri.replace("//", "/");
+
+		return new RibbonCommandContext(serviceId, verb, uri, retryable,
 				headers, params, requestEntity);
+	}
+
+	private ClientHttpResponse forward(RibbonCommandContext context) throws Exception {
+		Map<String, Object> info = this.helper.debug(context.getVerb(), context.getUri(),
+				context.getHeaders(), context.getParams(), context.getRequestEntity());
+
+		RibbonCommand command = ribbonCommandFactory.create(context);
 		try {
-			HttpResponse response = command.execute();
-			this.helper.appendDebug(info, response.getStatus(),
-					revertHeaders(response.getHeaders()));
+			ClientHttpResponse response = command.execute();
+			this.helper.appendDebug(info, response.getStatusCode().value(),
+					response.getHeaders());
 			return response;
 		}
 		catch (HystrixRuntimeException ex) {
@@ -141,15 +131,6 @@ public class RibbonRoutingFilter extends ZuulFilter {
 					.toString());
 		}
 
-	}
-
-	private MultiValueMap<String, String> revertHeaders(
-			Map<String, Collection<String>> headers) {
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		for (Entry<String, Collection<String>> entry : headers.entrySet()) {
-			map.put(entry.getKey(), new ArrayList<>(entry.getValue()));
-		}
-		return map;
 	}
 
 	private InputStream getRequestBody(HttpServletRequest request) {
@@ -171,26 +152,16 @@ public class RibbonRoutingFilter extends ZuulFilter {
 		return requestEntity;
 	}
 
-	private Verb getVerb(HttpServletRequest request) {
-		String sMethod = request.getMethod();
-		return getVerb(sMethod);
+	private String getVerb(HttpServletRequest request) {
+		String method = request.getMethod();
+		if (method == null)
+			return "GET";
+		return method;
 	}
 
-	private Verb getVerb(String sMethod) {
-		if (sMethod == null)
-			return Verb.GET;
-		try {
-			return Verb.valueOf(sMethod.toUpperCase());
-		}
-		catch (IllegalArgumentException e) {
-			return Verb.GET;
-		}
-	}
-
-	private void setResponse(HttpResponse resp) throws ClientException, IOException {
-		this.helper.setResponse(resp.getStatus(),
-				!resp.hasEntity() ? null : resp.getInputStream(),
-				revertHeaders(resp.getHeaders()));
+	private void setResponse(ClientHttpResponse resp) throws ClientException, IOException {
+		this.helper.setResponse(resp.getStatusCode().value(),
+				resp.getBody() == null ? null : resp.getBody(), resp.getHeaders());
 	}
 
 }
