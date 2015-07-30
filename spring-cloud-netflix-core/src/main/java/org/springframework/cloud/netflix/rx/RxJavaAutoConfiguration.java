bebd49612a44189b3206b637b29a0b7612950a08
Spencer Gibb <spencer@gibb.us>
2015-07-30 14:05:46 -0600

    Support returning Observable from Spring MVC Controllers.
    Via spring 4.2 new AsyncHandlerMethodReturnValueHandler
    
    fixes gh-231

---
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/rx/RxJavaAutoConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.netflix.rx;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import rx.Observable;
+
+/**
+ * @author Spencer Gibb
+ */
+@Configuration
+@ConditionalOnClass(Observable.class)
+public class RxJavaAutoConfiguration {
+
+	@Bean
+	public ObservableReturnValueHandler observableReturnValueHandler() {
+		return new ObservableReturnValueHandler();
+	}
+
+	@Bean
+	@ConditionalOnWebApplication
+	public WebMvcConfigurerAdapter observableMVCConfiguration() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+				returnValueHandlers.add(observableReturnValueHandler());
+			}
+		};
+	}
+}
