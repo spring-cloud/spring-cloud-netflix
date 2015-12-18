2bb6befbca5172086d16f790ae0e1f9f10ab6a35
Dave Syer <dsyer@pivotal.io>
2015-12-18 11:48:47 +0000

    Add vanilla integration tests

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -2,12 +2,24 @@
 
 set -o errexit
 
+mkdir -p target
+
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
 AT_WHAT_TO_TEST="EUREKA"
 AT_VERSION="1.1.0.BUILD-SNAPSHOT"
+
+cd target
 
 curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
 
 chmod +x runDockerAcceptanceTests.sh
 
 ./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
+
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/tests/master/scripts/runTests.sh"
+
+curl "${SCRIPT_URL}" --output runIntegrationTests.sh
+
+chmod +x runIntegrationTests.sh
+
+./runIntegrationTests.sh
