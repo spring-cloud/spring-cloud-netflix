fae2c5d1b5b9c92b890def222852c597172210c6
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-12-21 13:39:30 +0100

    Increasing polling interval for e2e tests even more

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -6,7 +6,7 @@ mkdir -p target
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
 AT_WHAT_TO_TEST="EUREKA"
-TEST_OPTS="-Dpresenting.poll.interval=3"
+TEST_OPTS="-Dpresenting.poll.interval=5"
 
 cd target
 
