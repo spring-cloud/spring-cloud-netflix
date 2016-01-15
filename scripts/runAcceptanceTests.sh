e4f81fb62ec32115c232c8a6a240a547c094f5ec
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-01-15 12:36:03 +0100

    Updated e2e scripts

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -4,16 +4,16 @@ set -o errexit
 
 mkdir -p target
 
-SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/removing_docker_compose_zookeeper/runAcceptanceTests.sh"
 AT_WHAT_TO_TEST="EUREKA"
 
 cd target
 
-curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
+curl "${SCRIPT_URL}" --output runAcceptanceTests.sh
 
-chmod +x runDockerAcceptanceTests.sh
+chmod +x runAcceptanceTests.sh
 
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -k 1
+./runAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -k 1
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/tests/master/scripts/runTests.sh"
 
