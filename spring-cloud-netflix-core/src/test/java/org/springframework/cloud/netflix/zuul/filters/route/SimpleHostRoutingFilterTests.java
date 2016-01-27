26520d1488158fd3b95f36be8a21181c728ce495
Andreas Kluth <mail@andreaskluth.net>
2016-01-27 22:46:39 +0100

    Moved away from System.properties. Added config object and used it in filter accordingly.

--- spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/zuul/filters/route/SimpleHostRoutingFilterTests.java
+++ spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/zuul/filters/route/SimpleHostRoutingFilterTests.java
@@ -2,60 +2,51 @@ package org.springframework.cloud.netflix.zuul.filters.route;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Properties;
-
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.HostRoutingFilterProperties;
 
 /**
  * @author Andreas Kluth
  */
 public class SimpleHostRoutingFilterTests {
 
-	private static final String ZUUL_MAX_HOST_CONNECTIONS = "zuul.max.host.connections";
-	private static final String ZUUL_MAX_ROUTE_CONNECTIONS = "zuul.max.route.connections";
-
-	@Before
-	@After
-	public void reset() {
-		Properties sysProps = System.getProperties();
-		sysProps.remove(ZUUL_MAX_HOST_CONNECTIONS);
-		sysProps.remove(ZUUL_MAX_ROUTE_CONNECTIONS);
-	}
-
 	@Test
-	public void maxPerRouteAndMaxTotalDiffer() {
-		setHostAndRouteConnectionProperties("100", "10");
-
-		PoolingHttpClientConnectionManager newConnectionManager = createFilter()
-				.newConnectionManager();
+	public void connectionPropertiesAreApplied() {
+		PoolingHttpClientConnectionManager newConnectionManager = createFilter(
+				withProperties(100, 10)).newConnectionManager();
 
 		assertEquals(100, newConnectionManager.getMaxTotal());
 		assertEquals(10, newConnectionManager.getDefaultMaxPerRoute());
 	}
 
 	@Test
-	public void defaults() {
-		PoolingHttpClientConnectionManager newConnectionManager = createFilter()
-				.newConnectionManager();
+	public void defaultPropertiesAreApplied() {
+		PoolingHttpClientConnectionManager newConnectionManager = createFilter(
+				new ZuulProperties()).newConnectionManager();
+
 		assertEquals(200, newConnectionManager.getMaxTotal());
 		assertEquals(20, newConnectionManager.getDefaultMaxPerRoute());
 	}
 
-	private void setHostAndRouteConnectionProperties(String maxPerHost,
-			String maxPerRoute) {
-		System.setProperty(ZUUL_MAX_HOST_CONNECTIONS, maxPerHost);
-		System.setProperty(ZUUL_MAX_ROUTE_CONNECTIONS, maxPerRoute);
+	private ZuulProperties withProperties(int maxPerHost, int maxPerRoute) {
+		ZuulProperties properties = new ZuulProperties();
+		properties.setHostRoutingFilter(
+				new HostRoutingFilterProperties(maxPerHost, maxPerRoute));
+		return properties;
 	}
 
-	private SpySimpleHostRoutingFilter createFilter() {
-
-		return new SpySimpleHostRoutingFilter();
+	private SpySimpleHostRoutingFilter createFilter(ZuulProperties properties) {
+		return new SpySimpleHostRoutingFilter(properties);
 	}
 
 	private static class SpySimpleHostRoutingFilter extends SimpleHostRoutingFilter {
+
+		public SpySimpleHostRoutingFilter(ZuulProperties properties) {
+			super(new ProxyRequestHelper(), properties);
+		}
 
 		@Override
 		public PoolingHttpClientConnectionManager newConnectionManager() {
