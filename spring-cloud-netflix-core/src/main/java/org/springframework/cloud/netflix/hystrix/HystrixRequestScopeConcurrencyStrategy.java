19f8befc2cb3f25a59ebcdd815a18042509433b3
Claudio Tasso <c.tasso@kion.it>
2016-10-05 15:10:37 +0200

    Fix for issue #1124

---
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/hystrix/HystrixRequestScopeConcurrencyStrategy.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.netflix.hystrix;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Passes the request attributes used by Spring from the parent ThreadLocal (the one which handles the request) to the child Thread
+ * (created by Hystrix). In fact, Hystrix executes wrapped code in a different thread pool, so session and request scoped Spring beans are
+ * not accessible; request and session attributes must be copied from the parent ThreadLocal to the Hystrix ThreadLocal.
+ * See the bugs below:
+ *
+ * https://github.com/spring-cloud/spring-cloud-netflix/issues/1124
+ * https://github.com/Netflix/Hystrix/issues/1162
+ *
+ *
+ * Created by ctasso on 30/09/2016.
+ *
+ * @author Claudio Tasso
+ */
+
+public class HystrixRequestScopeConcurrencyStrategy extends HystrixConcurrencyStrategy {
+    @Override
+    public <T> Callable<T> wrapCallable(Callable<T> callable) {
+
+        //retrieves current ThreadLocal request attributes
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+        return new WrapperCallable<T>(requestAttributes,callable);
+    }
+
+    private static class WrapperCallable<T> implements Callable<T>{
+
+        private RequestAttributes requestAttributes;
+        private Callable<T> target;
+
+        WrapperCallable(RequestAttributes requestAttributes, Callable<T> target){
+            this.requestAttributes = requestAttributes;
+            this.target = target;
+        }
+
+        @Override
+        public T call() throws Exception {
+            RequestContextHolder.setRequestAttributes(this.requestAttributes);
+
+            try{
+                return target.call();
+            }finally {
+                RequestContextHolder.resetRequestAttributes();
+            }
+        }
+    }
+}
---
+++ spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/hystrix/scope/HystrixRequestScopeTests.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.netflix.hystrix.scope;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests that a Hystrix command works properly using Spring beans with request scope.
+ *
+ * Created by ctasso on 05/10/2016.
+ *
+ * @author Claudio Tasso
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+@SpringBootTest(classes = HystrixRequestScopeTests.MyApplication.class)
+public class HystrixRequestScopeTests {
+
+    @Autowired
+    private MyService myService;
+
+    @SpringBootApplication
+    @Configuration
+    @EnableCircuitBreaker
+    public static class MyApplication{
+
+        @Bean
+        public MyService myService(){
+            return new MyService();
+        }
+
+        @Bean
+        @Scope(value="request",proxyMode= ScopedProxyMode.TARGET_CLASS)
+        public RequestService requestService(){
+            return new RequestService();
+        }
+
+    }
+
+    public  static class MyService {
+
+        @Autowired
+        private RequestService requestService;
+
+        @HystrixCommand
+        public String hello() {
+            return requestService.execute();
+        }
+    }
+
+    public static class RequestService {
+        public String execute(){
+            return "ciao";
+        }
+    }
+
+    @Test
+    public void testRequestScopedBean(){
+
+        Assert.assertEquals("ciao", myService.hello());
+    }
+}
