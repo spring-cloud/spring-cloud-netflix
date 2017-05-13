5931f3c1e87820d1acfff9f132723928013a7a1c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2017-05-13 18:06:38 +0700

    add feign client properties

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/FeignAutoConfiguration.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/FeignAutoConfiguration.java
@@ -25,6 +25,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.actuator.HasFeatures;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,6 +41,7 @@ import feign.okhttp.OkHttpClient;
  */
 @Configuration
 @ConditionalOnClass(Feign.class)
+@EnableConfigurationProperties({FeignClientProperties.class})
 public class FeignAutoConfiguration {
 
 	@Autowired(required = false)
---
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/FeignClientProperties.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.netflix.feign;
+
+import feign.Logger;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Eko Kurniawan Khannedy
+ */
+@Data
+@ConfigurationProperties("feign.client")
+public class FeignClientProperties {
+
+  private String defaultConfig = "default";
+
+  private Map<String, FeignClientConfiguration> config = new HashMap<>();
+
+  @Data
+  public static class FeignClientConfiguration {
+
+    private Logger.Level loggerLevel = Logger.Level.NONE;
+
+    private Integer connectTimeout = 10000;
+
+    private Integer readTimeout = 60000;
+
+    private String retryer;
+
+    private String errorDecoder;
+
+    private List<String> requestInterceptors;
+
+  }
+
+}
