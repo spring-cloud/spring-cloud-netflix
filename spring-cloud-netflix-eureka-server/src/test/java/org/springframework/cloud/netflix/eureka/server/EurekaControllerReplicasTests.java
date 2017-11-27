6c52269dc56f2f4fd5012d9ff77c261631bf1a07
saga <spring_holy@163.com>
2017-11-27 20:55:39 +0800

    Associated with this PR # 2466 (#2470)
    Use StringBuffer for String concatenation

--- spring-cloud-netflix-eureka-server/src/test/java/org/springframework/cloud/netflix/eureka/server/EurekaControllerReplicasTests.java
+++ spring-cloud-netflix-eureka-server/src/test/java/org/springframework/cloud/netflix/eureka/server/EurekaControllerReplicasTests.java
@@ -23,6 +23,15 @@ public class EurekaControllerReplicasTests {
 	String authList1 = "http://user:pwd@test1.com";
 	String authList2 = authList1 + ",http://user2:pwd2@test2.com";
 
+	String combinationAuthList1 = "http://test1.com,http://user2:pwd2@test2.com";
+	String combinationAuthList2 = "http://test3.com,http://user4:pwd4@test4.com";
+
+	String combinationNoAuthList1 = "http://test1.com,http://test2.com";
+	String combinationNoAuthList2 = "http://test3.com,http://test4.com";
+
+	String totalAutoList = combinationAuthList1 + "," + combinationAuthList2;
+	String totalNoAutoList = combinationNoAuthList1 + "," + combinationNoAuthList2;
+
 	String empty = new String();
 
 	private ApplicationInfoManager original;
@@ -78,6 +87,26 @@ public class EurekaControllerReplicasTests {
 		assertEquals(empty, results.get("unavailable-replicas"));
 		assertEquals(noAuthList1, results.get("available-replicas"));
 		assertEquals(noAuthList2, results.get("registered-replicas"));
+
+	}
+
+	@Test
+	public void testFilterReplicasAuthWithCombinationList() throws Exception {
+		Map<String, Object> model = new HashMap<>();
+		StatusInfo statusInfo = StatusInfo.Builder.newBuilder()
+			.add("registered-replicas", totalAutoList)
+			.add("available-replicas", combinationAuthList1)
+			.add("unavailable-replicas", combinationAuthList2)
+			.withInstanceInfo(instanceInfo).build();
+		EurekaController controller = new EurekaController(null);
+
+		controller.filterReplicas(model, statusInfo);
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> results = (Map<String, String>) model.get("applicationStats");
+		assertEquals(totalNoAutoList, results.get("registered-replicas"));
+		assertEquals(combinationNoAuthList1, results.get("available-replicas"));
+		assertEquals(combinationNoAuthList2, results.get("unavailable-replicas"));
 
 	}
 
