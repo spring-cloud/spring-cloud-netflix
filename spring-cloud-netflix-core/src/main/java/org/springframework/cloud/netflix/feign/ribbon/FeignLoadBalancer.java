c34b76e1ec5a9013c3ac287bc6c44bf86eedd17d
ileler <ileler@qq.com>
2017-12-15 11:23:22 +0800

    exclude other file

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/ribbon/FeignLoadBalancer.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/ribbon/FeignLoadBalancer.java
@@ -105,7 +105,7 @@ public class FeignLoadBalancer extends
 		return super.reconstructURIWithServer(server, uri);
 	}
 
-	protected static class RibbonRequest extends ClientRequest implements Cloneable {
+	static class RibbonRequest extends ClientRequest implements Cloneable {
 
 		private final Request request;
 		private final Client client;
@@ -169,7 +169,7 @@ public class FeignLoadBalancer extends
 		}
 	}
 
-	protected static class RibbonResponse implements IResponse {
+	static class RibbonResponse implements IResponse {
 
 		private final URI uri;
 		private final Response response;
