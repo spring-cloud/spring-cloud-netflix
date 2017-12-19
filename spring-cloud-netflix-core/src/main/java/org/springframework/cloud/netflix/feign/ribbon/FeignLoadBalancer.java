90094b6b9a49581cb503c509d8e1bc01a9982dc3
Kerwin Bryant <ileler@qq.com>
2017-12-20 03:43:39 +0800

    public FeignLoadBalancer.RibbonRequest and FeignLoadBalancer.RibbonResponse to the user to customize their needs (#2536)

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/ribbon/FeignLoadBalancer.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/ribbon/FeignLoadBalancer.java
@@ -111,7 +111,7 @@ public class FeignLoadBalancer extends
 		return super.reconstructURIWithServer(server, uri);
 	}
 
-	static class RibbonRequest extends ClientRequest implements Cloneable {
+	protected static class RibbonRequest extends ClientRequest implements Cloneable {
 
 		private final Request request;
 		private final Client client;
@@ -175,7 +175,7 @@ public class FeignLoadBalancer extends
 		}
 	}
 
-	static class RibbonResponse implements IResponse {
+	protected static class RibbonResponse implements IResponse {
 
 		private final URI uri;
 		private final Response response;
