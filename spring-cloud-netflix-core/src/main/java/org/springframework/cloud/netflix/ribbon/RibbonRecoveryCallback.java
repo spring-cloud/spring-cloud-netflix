35d0253761f2c2e94d11ac457e299c70cf2ebeba
liliyuan <liliyuan@it.com>
2018-01-11 10:18:42 +0800

    add copyright header

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/ribbon/RibbonRecoveryCallback.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/ribbon/RibbonRecoveryCallback.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.cloud.netflix.ribbon;
 
 import org.springframework.cloud.client.loadbalancer.RetryableStatusCodeException;
@@ -7,6 +22,9 @@ import org.springframework.retry.RetryException;
 
 import java.net.URI;
 
+/**
+ * @author LiYuan Lee
+ */
 public abstract class RibbonRecoveryCallback<T, R> implements RecoveryCallback<T> {
 
 	protected abstract T createResponse(R response, URI uri);
