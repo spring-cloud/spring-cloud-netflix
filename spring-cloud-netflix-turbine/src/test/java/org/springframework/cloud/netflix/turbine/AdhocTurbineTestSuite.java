bc4dfc6b5b2f1a108d6311df2719b95ecedbb3ab
Spencer Gibb <spencer@gibb.us>
2018-10-02 13:27:08 -0400

    Fixes broken test

---
+++ spring-cloud-netflix-turbine/src/test/java/org/springframework/cloud/netflix/turbine/AdhocTurbineTestSuite.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.netflix.turbine;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		// org.springframework.cloud.netflix.turbine.EurekaBasedTurbineClustersProviderTest.class,
+		// org.springframework.cloud.netflix.turbine.ConfigurationBasedTurbineClustersProviderTest.class,
+		// org.springframework.cloud.netflix.turbine.CommonsInstanceDiscoveryTests.class,
+		// org.springframework.cloud.netflix.turbine.TurbineAggregatorPropertiesTest.class,
+		// org.springframework.cloud.netflix.turbine.TurbineHttpTests.class,
+		// org.springframework.cloud.netflix.turbine.EurekaInstanceDiscoveryTests.class,
+})
+@Ignore
+public class AdhocTurbineTestSuite {
+}
