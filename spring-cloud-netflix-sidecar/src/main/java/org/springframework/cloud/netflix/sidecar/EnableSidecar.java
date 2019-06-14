87bae6f5f608e352204e419f7d3e21dd4fc1a364
Greg Adams <gadams@gmail.com>
2019-06-14 09:58:49 -0700

    conditional sidecar autoconfig EurekaInstanceConfigBean
    fix for gh-3545

--- spring-cloud-netflix-sidecar/src/main/java/org/springframework/cloud/netflix/sidecar/EnableSidecar.java
+++ spring-cloud-netflix-sidecar/src/main/java/org/springframework/cloud/netflix/sidecar/EnableSidecar.java
@@ -34,7 +34,7 @@ import org.springframework.context.annotation.Import;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import(SidecarConfiguration.class)
+@Import(SidecarMarkerConfiguration.class)
 public @interface EnableSidecar {
 
 }
