a609e4bb80f298e1c5673df67adab59a494379e1
spencergibb <spencer@gibb.us>
2020-05-13 22:01:51 -0400

    Proof of concept for using http to look up config server.
    This eliminates the oddness of having multiple discovery client beans and related behavior.
    
    Fixes gh-3795
    
    See https://github.com/spring-cloud/spring-cloud-gateway/issues/1514

--- spring-cloud-netflix-eureka-client/src/main/java/org/springframework/cloud/netflix/eureka/config/EurekaDiscoveryClientConfigServiceBootstrapConfiguration.java
+++ spring-cloud-netflix-eureka-client/src/main/java/org/springframework/cloud/netflix/eureka/config/EurekaDiscoveryClientConfigServiceBootstrapConfiguration.java
@@ -16,15 +16,38 @@
 
 package org.springframework.cloud.netflix.eureka.config;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.AzToRegionMapper;
+import com.netflix.discovery.DNSBasedAzToRegionMapper;
+import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.discovery.InstanceRegionChecker;
+import com.netflix.discovery.PropertyBasedAzToRegionMapper;
+import com.netflix.discovery.endpoint.EndpointUtils;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.discovery.shared.resolver.DefaultEndpoint;
+import com.netflix.discovery.shared.transport.EurekaHttpResponse;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.client.ReactiveCommonsClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
-import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
-import org.springframework.cloud.netflix.eureka.reactive.EurekaReactiveDiscoveryClientConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateEurekaHttpClient;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateTransportClientFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * Eureka-specific helper for config client that wants to lookup the config server via
@@ -36,13 +59,100 @@ import org.springframework.context.annotation.Import;
 @ConditionalOnProperty(value = "spring.cloud.config.discovery.enabled",
 		matchIfMissing = false)
 @Configuration(proxyBeanMethods = false)
-@Import({ EurekaDiscoveryClientConfiguration.class, // this emulates
-		// @EnableDiscoveryClient, the import
-		// selector doesn't run before the
-		// bootstrap phase
-		EurekaClientAutoConfiguration.class,
-		EurekaReactiveDiscoveryClientConfiguration.class,
-		ReactiveCommonsClientAutoConfiguration.class })
 public class EurekaDiscoveryClientConfigServiceBootstrapConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean(value = EurekaClientConfig.class,
+			search = SearchStrategy.CURRENT)
+	public EurekaClientConfigBean eurekaClientConfigBean() {
+		return new EurekaClientConfigBean();
+	}
+
+	@Bean
+	public RestTemplateEurekaHttpClient configDiscoveryWebClientEurekaHttpClient(
+			EurekaClientConfigBean config) {
+		List<String> urls = EndpointUtils.getServiceUrlsFromConfig(config, "unknown",
+				true);
+		String url = urls.get(0);
+		return (RestTemplateEurekaHttpClient) new RestTemplateTransportClientFactory()
+				.newClient(new DefaultEndpoint(url));
+	}
+
+	private boolean isSuccessful(EurekaHttpResponse<Applications> response) {
+		return HttpStatus.resolve(response.getStatusCode()).is2xxSuccessful();
+	}
+
+	@Bean
+	// TODO: only need blocking since ConfigServerInstanceProvider expects only it.
+	public DiscoveryClient configDiscoveryDiscoveryClient(
+			RestTemplateEurekaHttpClient client, EurekaClientConfig clientConfig) {
+		return new DiscoveryClient() {
+
+			@Override
+			public String description() {
+				return "configDiscoveryDiscoveryClient";
+			}
+
+			@Override
+			public List<ServiceInstance> getInstances(String serviceId) {
+				AzToRegionMapper azToRegionMapper;
+				if (clientConfig.shouldUseDnsForFetchingServiceUrls()) {
+					azToRegionMapper = new DNSBasedAzToRegionMapper(clientConfig);
+				}
+				else {
+					azToRegionMapper = new PropertyBasedAzToRegionMapper(clientConfig);
+				}
+				String localRegion = null;
+				try {
+					Constructor<InstanceRegionChecker> constructor = ReflectionUtils
+							.accessibleConstructor(InstanceRegionChecker.class,
+									AzToRegionMapper.class, String.class);
+
+					InstanceRegionChecker instanceRegionChecker = constructor
+							.newInstance(azToRegionMapper, clientConfig.getRegion());
+					localRegion = instanceRegionChecker.getLocalRegion();
+				}
+				catch (Exception e) {
+					ReflectionUtils.rethrowRuntimeException(e);
+				}
+
+				EurekaHttpResponse<Applications> response = client
+						.getApplications(localRegion);
+				List<ServiceInstance> instances = new ArrayList<>();
+				if (!isSuccessful(response) || response.getEntity() == null) {
+					return instances;
+				}
+
+				Applications applications = response.getEntity();
+				applications.shuffleInstances(clientConfig.shouldFilterOnlyUpInstances());
+				List<InstanceInfo> infos = applications
+						.getInstancesByVirtualHostName(serviceId);
+				for (InstanceInfo info : infos) {
+					instances.add(new EurekaServiceInstance(info));
+				}
+				return instances;
+			}
+
+			@Override
+			public List<String> getServices() {
+				EurekaHttpResponse<Applications> response = client.getApplications();
+				if (!isSuccessful(response) || response.getEntity() == null) {
+					return Collections.emptyList();
+				}
+				List<Application> registered = response.getEntity()
+						.getRegisteredApplications();
+				List<String> names = new ArrayList<>();
+				for (Application app : registered) {
+					if (app.getInstances().isEmpty()) {
+						continue;
+					}
+					names.add(app.getName().toLowerCase());
+
+				}
+				return names;
+			}
+
+		};
+	}
 
 }
