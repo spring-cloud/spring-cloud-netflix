a568481baa3375ce2025d6de168620ef547febc0
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-11-17 20:52:06 +0100

    Revert "Modify build script."
    This reverts commit 80a0b536120d9715b4d3779eef8233d581a4afd5.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,5 @@
 
 set -o errexit
 
-cd spring-cloud-netflix-hystrix-contract
-../mvnw clean install -B -Pdocs ${@}
-cd ..
+(cd spring-cloud-netflix-hystrix-contract && ../mvnw clean install -B -Pdocs ${@})
 ./mvnw clean install -B -Pdocs ${@}
