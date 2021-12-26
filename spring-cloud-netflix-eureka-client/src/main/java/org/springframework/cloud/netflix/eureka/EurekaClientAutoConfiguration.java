88003d9eab71f0163b0330f40a324d187c4f2493
Swapnil Singh <swappp7singh@gmail.com>
2021-12-26 14:50:38 +0530

    Added support for eureka to publish custom port to registry

--- spring-cloud-netflix-eureka-client/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientAutoConfiguration.java
+++ spring-cloud-netflix-eureka-client/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientAutoConfiguration.java
@@ -130,7 +130,8 @@ public class EurekaClientAutoConfiguration {
 		boolean isSecurePortEnabled = Boolean.parseBoolean(getProperty("eureka.instance.secure-port-enabled"));
 
 		String serverContextPath = env.getProperty("server.servlet.context-path", "/");
-		int serverPort = Integer.parseInt(env.getProperty("server.port", env.getProperty("port", "8080")));
+		int serverPort = Integer.parseInt(env.getProperty("eureka.instance.custom-port",
+				env.getProperty("server.port", env.getProperty("port", "8080"))));
 
 		Integer managementPort = env.getProperty("management.server.port", Integer.class);
 
