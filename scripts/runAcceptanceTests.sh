83c74b00179644e8501e64694d774cad5a673c0c
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2022-04-20 13:05:45 +0200

    Update runAcceptanceTests.sh

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 mkdir -p target
 
-SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/main/runAcceptanceTests.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/2020.0.x/runAcceptanceTests.sh"
 AT_WHAT_TO_TEST="EUREKA"
 
 cd target
