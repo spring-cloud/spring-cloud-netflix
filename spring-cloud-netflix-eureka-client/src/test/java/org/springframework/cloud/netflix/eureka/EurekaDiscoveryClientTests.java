5148c1a6df1466bf1055648c3323397c1f06e144
Mohamed Macow <mmacovv@gmail.com>
2025-11-02 22:46:16 +0100

    Implement test for probe()
    Signed-off-by: Mohamed Macow <mmacovv@gmail.com>

---
+++ spring-cloud-netflix-eureka-client/src/test/java/org/springframework/cloud/netflix/eureka/EurekaDiscoveryClientTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.netflix.eureka;
+
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Applications;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * @author Mohamed Macow
+ */
+@ExtendWith(MockitoExtension.class)
+class EurekaDiscoveryClientTests {
+
+	@Mock
+	private EurekaClient eurekaClient;
+
+	@InjectMocks
+	private EurekaDiscoveryClient client;
+
+	@Test
+	void shouldCompleteProbeWhenClientHealthy() {
+		when(eurekaClient.getApplications()).thenReturn(new Applications());
+		assertDoesNotThrow(() -> client.probe());
+	}
+
+	@Test
+	void shouldThrowProbeWhenClientThrows() {
+		RuntimeException eurekaException = new RuntimeException("exception");
+		when(eurekaClient.getApplications()).thenThrow(eurekaException);
+
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> client.probe());
+		assertThat(eurekaException.getMessage()).isEqualTo(thrown.getMessage());
+	}
+
+}
